781bc1412103c8c3f06de34665d917dac40df47a
Aylin <aylin.a_@live.de>
2021-10-06 18:57:14 +0200

    New Sheet "Drop_down" Installation of tkinter, created window for drop down menu

--- Drop_down.py
+++ Drop_down.py
@@ -1,5 +1,25 @@
-import tkinter
+import tkinter as tk
 from tkinter import *
 
-window= tkinter.Tk()
-window.mainloop()
+
+
+
+OptionList = [
+"Water",
+"Sanitation",
+"Hygiene",
+
+]
+
+app = tk.Tk()
+
+app.geometry('100x200')
+
+variable = tk.StringVar(app)
+variable.set(OptionList[0])
+
+opt = tk.OptionMenu(app, variable, *OptionList)
+opt.config(width=90, font=('Helvetica', 12))
+opt.pack()
+
+app.mainloop()
